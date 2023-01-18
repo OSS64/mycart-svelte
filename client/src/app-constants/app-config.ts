9632237e36bf8ef05f391cf16b4e616558fe8d7d
Jobayer Hossain <jobayerhossain977@Gmail.com>
2023-01-18 23:02:25 +0600

    24-12: Added

--- client/src/app-constants/app-config.ts
+++ client/src/app-constants/app-config.ts
@@ -1,6 +1,6 @@
 export const AppConstants = {
 	baseApi: 'http://localhost:3000',
-	apiBase: 'http://127.0.0.1:5000',
+	apiBase: 'http://localhost:5000',
 	amazonNoImage: 'https://m.media-amazon.com/images/I/01RPy1SfeqL._SS500_.gif',
 	formatterConfig: {
 		locale: 'en-IN',
